b7c9e0d5ed657362267d332a5351e0ea4a61160d
parth-17512 <parth.singh.17512@gmail.com>
2025-04-17 07:56:45 +0530

    Initial commit

---
+++ lib/quiz.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/widgets.dart';
+
+class Quiz extends StatefulWidget {}
+
+class _QuizState extends State<Quiz>{
+  @override
+  Widget build( context) {
+    return
+  }
+}
--- lib/start_screen.dart
+++ lib/start_screen.dart
@@ -6,7 +6,35 @@ class StartScreen extends StatelessWidget {
   @override
   Widget build(context) {
     return Center(
-      child: Column(children: [Image.asset('assets/images/quiz-logo.png')]),
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Image.asset(
+            'assets/images/quiz-logo.png',
+            width: 300,
+            color: const Color.fromARGB(150, 243, 228, 228),
+          ),
+
+          // Opacity(
+          //   opacity: 0.6,
+          //   child: Image.asset('assets/images/quiz-logo.png', width: 300),
+          // ),
+          const SizedBox(height: 80),
+          const Text(
+            'Learn flutter the fun way!',
+            style: TextStyle(color: Colors.white, fontSize: 24),
+          ),
+          const SizedBox(height: 30),
+          OutlinedButton.icon(
+            onPressed: () {
+              
+            },
+            style: OutlinedButton.styleFrom(foregroundColor: Colors.white),
+            icon: Icon(Icons.whatshot),
+            label: const Text('Start Quiz'),
+          ),
+        ],
+      ),
     );
   }
 }
